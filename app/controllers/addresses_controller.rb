c41bfd06190968508281f1620f9b0888cbf3b1ea
Guilherme Mattos <mttsguil@gmail.com>
2022-11-29 10:31:40 -0300

    add: phone CRUD

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,39 +1,40 @@
 class AddressesController < ApplicationController
   before_action :set_contact
 
+  # DELETE /contacts/1/address
   def destroy
     @contact.address.destroy
   end
 
-  def create 
+  # POST /contacts/1/address
+  def create
     @contact.address = Address.new(address_params)
 
-    if @contact.save 
+    if @contact.save
       render json: @contact.address, status: :created, location: contact_address_url(@contact)
-    else 
+    else
       render json: @contact.errors, status: :unprocessable_entity
     end
   end
 
-  def update 
+  # PATCH /contacts/1/address
+  def update
     if @contact.address.update(address_params)
       render json: @contact.address
-    else 
+    else
       render json: @contact.errors, status: :unprocessable_entity
     end
-
   end
- 
-  # GET /phones/1
+
+  # GET /contacts/1/address
   def show
     render json: @contact.address
   end
 
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_contact
-        @contact = Contact.find(params[:contact_id])
+      @contact = Contact.find(params[:contact_id])
     end
 
     def address_params
