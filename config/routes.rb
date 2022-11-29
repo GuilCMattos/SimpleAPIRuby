35d75f46c58790939bbdc86d491bedbc5b977d3b
Guilherme Mattos <mttsguil@gmail.com>
2022-11-29 12:44:21 -0300

    add: versions controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,26 @@ Rails.application.routes.draw do
  # resources :auths, only: [:create]
   resources :kinds
 
-  resources :contacts do 
+  scope module: 'v1' do
+      
+      resources :contacts do 
+      resource :kind, only: [:show]
+      resource :kind, only: [:show], path: 'relationships/kind'
+
+      resource :phones, only: [:show]
+      resource :phones, only: [:show], path: 'relationships/phones'
+
+      resource :phone, only: [:update, :create, :destroy ]
+      resource :phone, only: [:update, :create, :destroy], path: 'relationships/phones'
+
+      resource :address, only: [:show, :update, :create]
+      resource :address, only: [:show, :update, :create], path: 'relationships/address'
+    end
+  end
+
+  scope module: 'v2' do
+      
+    resources :contacts do 
     resource :kind, only: [:show]
     resource :kind, only: [:show], path: 'relationships/kind'
 
@@ -17,5 +36,6 @@ Rails.application.routes.draw do
     resource :address, only: [:show, :update, :create]
     resource :address, only: [:show, :update, :create], path: 'relationships/address'
   end
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
